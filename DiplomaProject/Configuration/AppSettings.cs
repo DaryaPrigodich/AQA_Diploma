a424fd57734676c47d833dcae64d76f85bbfc5ab
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-03 18:58:35 +0300

    Implemented Configurator to retrieve data from appsettings.json

---
+++ DiplomaProject/Configuration/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace DiplomaProject.Configuration;
+
+public class AppSettings
+{
+    public string ApiUrl { get; set; }
+    public string UiUrl { get; set; }
+}
---
+++ DiplomaProject/Configuration/User.cs
@@ -0,0 +1,10 @@
+using DiplomaProject.Models.Enum;
+
+namespace DiplomaProject.Configuration;
+
+public record User
+{
+    public UserType UserType { get; set; }
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
