3d6937168ab5c3484857b25b3689f3384bb2cbd9
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-03 23:29:35 +0300

    DIPLOMA-1: Implemented overview page

---
+++ DiplomaProject/Pages/ProjectOverviewPage.cs
@@ -0,0 +1,22 @@
+﻿using DiplomaProject.Configuration;
+using OpenQA.Selenium;
+
+namespace DiplomaProject.Pages;
+
+public class ProjectOverviewPage : BasePage
+{
+    private const string Endpoint = "/projects";
+    
+    public ProjectOverviewPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    public ProjectOverviewPage(IWebDriver driver) : base(driver, false)
+    {
+    }
+
+    protected override void OpenPage()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.UiUrl + Endpoint);
+    }
+}
