4d3ef7feee9e6cd702a5a20f53706ec65c441c89
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-03 21:03:36 +0300

    NO CASE: Added wait service to improve reliability

---
+++ DiplomaProject/Services/UI/WaitService.cs
@@ -0,0 +1,18 @@
+﻿using DiplomaProject.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DiplomaProject.Services.UI;
+
+public class WaitService
+{
+    private IWebDriver _driver;
+
+    private readonly WebDriverWait _waitService;
+
+    public WaitService(IWebDriver driver)
+    {
+        _driver = driver;
+        _waitService = new WebDriverWait(_driver, TimeSpan.FromSeconds(Configurator.AppSettings.WaitTimeout));
+    }
+}
