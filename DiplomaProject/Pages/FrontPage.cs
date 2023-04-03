82805ecac9fca267fcf9ee3b57cd720fd27db5ac
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-03 23:02:19 +0300

    DIPLOMA-1: Implement front page

---
+++ DiplomaProject/Pages/FrontPage.cs
@@ -0,0 +1,25 @@
+﻿using DiplomaProject.Configuration;
+using OpenQA.Selenium;
+
+namespace DiplomaProject.Pages;
+
+public class FrontPage: BasePage
+{
+    private const string Endpoint = "";
+    
+    private IWebElement LoginButton => Driver.FindElement(By.Id("signin"));
+    
+    public FrontPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+    
+    protected override void OpenPage()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.UiUrl + Endpoint);
+    }
+    
+    public void ClickLoginButton()
+    {
+        LoginButton.Click();
+    }
+}
