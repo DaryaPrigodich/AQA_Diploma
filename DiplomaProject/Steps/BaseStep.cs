50b81f13edf79705d7c45d103ed93beb2b166709
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-03 23:22:37 +0300

    DIPLOMA-1: Implemented base step

---
+++ DiplomaProject/Steps/BaseStep.cs
@@ -0,0 +1,20 @@
+﻿using DiplomaProject.Pages;
+using OpenQA.Selenium;
+
+namespace DiplomaProject.Steps;
+
+public class BaseStep
+{
+    protected IWebDriver Driver;
+    
+    protected FrontPage FrontPage;
+    protected LoginPage LoginPage;
+
+    public BaseStep(IWebDriver driver)
+    {
+        Driver = driver;
+        
+        FrontPage = new FrontPage(Driver,true);
+        LoginPage = new LoginPage(Driver,false);
+    }
+}
