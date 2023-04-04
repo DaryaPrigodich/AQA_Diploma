7c7236a73409f67b531e27dbd6d2de268bda5a7a
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-04 13:48:13 +0300

    DIPLOMA-3: Implemented project page

---
+++ DiplomaProject/Pages/ProjectPage.cs
@@ -0,0 +1,35 @@
+﻿using DiplomaProject.Configuration;
+using DiplomaProject.Wrappers;
+using OpenQA.Selenium;
+
+namespace DiplomaProject.Pages;
+
+public class ProjectPage : BasePage
+{
+    private const string Endpoint = "project/";
+    
+    private Button AddFilter => new (Driver, By.ClassName("add-filter-button"));
+    private UiElement FilterOptionsDropDown => new (Driver, By.XPath("//*[@class='filters-menu WHRMzV']"));
+    private UiElement PriorityOptionsCheckBox => new (Driver, By.XPath("//*[@class='filter-checkboxes']"));
+    private UiElement MissingMatchingCasesMessage => new (Driver, By.XPath("//*[contains(text(),'not found')]"));
+
+    public ProjectPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+    
+    public ProjectPage(IWebDriver driver) : base(driver,false)
+    {
+    }
+    
+    protected override void OpenPage()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.UiUrl + Endpoint);
+    }
+
+    public ProjectPage OpenFilterOptions()
+    {
+        AddFilter.Click();
+
+        return this;
+    }
+}
