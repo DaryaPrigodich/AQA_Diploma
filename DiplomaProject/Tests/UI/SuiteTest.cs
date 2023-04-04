0dd6eff4a41a8a205994bf203c1d0271faacf535
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-04 15:20:47 +0300

    DIPLOMA-4: Implemented pre-/post-condition steps

---
+++ DiplomaProject/Tests/UI/SuiteTest.cs
@@ -0,0 +1,30 @@
+﻿using DiplomaProject.Configuration;
+using DiplomaProject.Fakers;
+using DiplomaProject.Models;
+using DiplomaProject.Pages;
+using NUnit.Framework;
+
+namespace DiplomaProject.Tests.UI;
+
+public class SuiteTest : BaseUiTest
+{
+    private Project _project = null!;
+
+    private ProjectOverviewPage _projectOverviewPage = null!;
+    
+    [SetUp]
+    public void SetUpPreconditionSteps()
+    {
+        _project = new ProjectFaker().Generate();
+        ProjectService.CreateProject(_project);
+       
+        _projectOverviewPage = LoginSteps
+            .LoginWithValidCredentials(Configurator.Admin.Username, Configurator.Admin.Password);
+    }
+
+    [TearDown]
+    public void SetUpPostConditionSteps()
+    {
+        ProjectService.DeleteProject(_project.Code.ToUpper());
+    }
+}
