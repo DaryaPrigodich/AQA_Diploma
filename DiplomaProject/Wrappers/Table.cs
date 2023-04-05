ce85b6921a967d646975b730b230bdad1c593152
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-05 13:14:43 +0300

    NO CASE: Added character limit for project faker

--- DiplomaProject/Wrappers/Table.cs
+++ DiplomaProject/Wrappers/Table.cs
@@ -11,42 +11,13 @@ public class Table
         _uiElement = new UiElement(driver, by);
     }
     
+    public bool Displayed => _uiElement.Displayed;
+    
     private IWebElement FindElement(By by)
     {
         return _uiElement.FindElement(by);
     }
     
-    public bool Displayed => _uiElement.Displayed;
-    
-    public IWebElement GetProjectByTittle(string projectTittle) => FindElement(By.XPath($"//tbody//a[text()='{projectTittle}']"));
-
-    
-    
-    
-   
-
-    /*public int CountRow()
-    {
-        return _rowList.Count;
-    }
-
-    private ReadOnlyCollection<IWebElement> Headers => _uiElement.FindElements(By.TagName("th"));
-    private ReadOnlyCollection<IWebElement> Rows => _uiElement.FindElements(By.XPath("//tbody/tr"));
-    private ReadOnlyCollection<IWebElement> Cells(IWebElement row) => row.FindElements(By.TagName("td"));
-
-    public IWebElement GetElementFromCell(string columnHeader, string columnValue, string targetColumnHeader)
-    {
-        var indexColumnHeader = Headers.TakeWhile(header => !header.Text.Normalize().Equals(columnHeader)).Count();
-        var indexTargetColumnHeader = Headers.TakeWhile(header => !header.Text.Normalize().Equals(targetColumnHeader)).Count();
-
-        foreach (var row in Rows)
-        {
-            var cells = Cells(row);
-            if (cells[indexColumnHeader].Text.Normalize().Equals(columnValue))
-            {
-                return cells[indexTargetColumnHeader];
-            }
-        }
-        return null;
-    }*/
+    public IWebElement GetProjectByTittle(string projectTittle) => 
+        FindElement(By.XPath($"//tbody//a[text()='{projectTittle}']"));
 }
