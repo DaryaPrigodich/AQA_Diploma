3f948b64dd48dae320d78ef714833763cacca9e0
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-04-06 11:49:29 +0300

    DIPLOMA-16: Created milestone faker to generate random data

---
+++ DiplomaProject/Fakers/MilestoneFaker.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using DiplomaProject.Models;
+
+namespace DiplomaProject.Fakers;
+
+public class MilestoneFaker : Faker<Milestone>
+{
+    public MilestoneFaker(int lengthOfTitle)
+    {
+        RuleFor(c => c.Title, f => f.Lorem.Letter(lengthOfTitle));
+        RuleFor(c => c.Description, f => f.Company.CatchPhrase());
+    }
+}
